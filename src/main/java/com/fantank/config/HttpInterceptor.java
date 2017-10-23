bea162cee60c8d69e032f60afbd020a0acb6c94d
sureshkumar <sureshkumar861@gmail.com>
2017-10-23 00:04:12 -0700

    Added https support

---
+++ src/main/java/com/fantank/config/HttpInterceptor.java
@@ -0,0 +1,22 @@
+package com.fantank.config;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.apache.commons.lang3.StringUtils;
+
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
+		if(request.getHeader("X-Forwarded-Proto") == null ) {
+			return true;
+		}
+		if(!StringUtils.equalsIgnoreCase(request.getHeader("X-Forwarded-Proto"), "https")){
+			response.sendRedirect(StringUtils.replaceFirst(request.getRequestURL().toString(), "http://", "https://"));
+			return false;
+		}
+		return true;
+	}
+
+}
